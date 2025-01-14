b69a50c26ae5937a0482911d390c39d70eaa06c0
Gnartiel <105764822+Gnartiel@users.noreply.github.com>
2025-01-14 15:00:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,7 @@
-const canvas = document.getElementById('fireworks');
-const ctx = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
 const audioElement = document.getElementById('music');
-canvas.addEventListener("click", function(event) {
+document.addEventListener("click", function(event) {
     if (event.target) {                
         audioElement.muted = false;
-        audioElement.play();
+        audioElement.play();  
     }
 });
-
-class Firework {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.radius = Math.random() * 3 + 2;
-    this.color = `hsl(${Math.random() * 360}, 100%, 50%)`;
-    this.life = 100;
-    this.dx = Math.random() * 4 - 2;
-    this.dy = Math.random() * 4 - 2;
-  }
-  update() {
-    this.x += this.dx;
-    this.y += this.dy;
-    this.life--;
-  }
-  draw() {
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-    ctx.fillStyle = this.color;
-    ctx.fill();
-  }
-}
-
-let fireworks = [];
-function addFirework(x, y) {
-  for (let i = 0; i < 50; i++) {
-    fireworks.push(new Firework(x, y));
-  }
-}
-
-function animate() {
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  fireworks.forEach((firework, i) => {
-    firework.update();
-    firework.draw();
-    if (firework.life <= 0) fireworks.splice(i, 1);
-  });
-  requestAnimationFrame(animate);
-}
-
-
-canvas.addEventListener('click', (e) => {
-  addFirework(e.clientX, e.clientY);
-});
-
-setInterval(() => {
-  addFirework(Math.random() * canvas.width, Math.random() * canvas.height);
-}, 1000);
-
-animate();
